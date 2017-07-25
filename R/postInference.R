930584000a54847f6bb204508440e0848cce8f90
trainorp <patrick.trainor@louisville.edu>
2017-07-25 13:24:33 -0400

    committ message

--- R/postInference.R
+++ R/postInference.R
@@ -3,11 +3,25 @@
 #' Posterior distributions
 #' 
 #' @export
-postInference<-function(x) UseMethod("postInference")
+posteriorCI<-function(x,...) UseMethod("posteriorCI")
 
 #' @export
-postInference.BayesianGLasso<-function(x)
+posteriorCI.BayesianGLasso<-function(x,parameters="Omega")
 {
+  # Check parameters
+  if(! parameters %in% c("Omega","Sigma"))
+  {
+    stop("Parameters argument must be Omega (concentration matrix) or 
+         Sigma (covariance matrix)")
+  }
+  
+  # Discard burn in:
+  x2<-x
+  x2$Sigmas<-x2$Sigmas[(x2$burnIn+1):length(x2$Sigmas)]
+  x2$Omegas<-x2$Omegas[(x2$burnIn+1):length(x2$Omegas)]
+  if(!is.null(x2$lambdas)) x2$lambdas<-x2$lambdas[(x2$burnIn+1):length(x2$lambdas)]
+  
+  print(x$burnIn)
   return(x$lambdas)
 }
 
